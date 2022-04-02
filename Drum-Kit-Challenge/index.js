58c4c6bfe68765e9dc7364d5fe998f8c9a20dbfe
Morgan Katjounis <morgan.katjounis@merkur.ca>
2022-04-02 11:10:51 -0400

    Finished the Drum Kit Challenge w/ jQuery

---
+++ Drum-Kit-Challenge/index.js
@@ -0,0 +1,52 @@
+
+$(".drum").click(function(){
+    let key = this.getAttribute("class").replace(" drum","");
+    playSound(key)
+    buttonAnimation(key)
+});
+
+$(document).keypress(function(e){
+    playSound(e.key)
+    buttonAnimation(e.key)
+});
+
+
+function playSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+    }
+}
+
+function buttonAnimation(key){
+    $("." + key).addClass("pressed");
+    setTimeout(function() {
+        $("." + key).removeClass("pressed");
+    }, 100);
+}
